090299ae0979b04efe421228476dd5aa9fc27879
nicolaiemig <nicolai.emig@amseegym.de>
2022-06-22 16:30:27 +0200

    Display IP Address upon connecting to Network

--- src/autoconnect.cpp
+++ src/autoconnect.cpp
@@ -5,4 +5,5 @@ void autoconnectSetup(){
   WiFiManager wifiManager;
   wifiManager.autoConnect("sis wifi");
   Serial.println("Connected to WiFi " + WiFi.SSID());
+  Serial.println("IP: " + WiFi.localIP().toString());
 }
