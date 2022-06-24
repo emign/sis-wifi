7bdb08cf5581a5a591ee096c4468e547234ada3a
nicolaiemig <nicolai.emig@amseegym.de>
2022-06-24 09:15:45 +0200

    Output value GET paramater on access

--- src/webserver.cpp
+++ src/webserver.cpp
@@ -10,33 +10,34 @@
 static AsyncWebServer server(80);
 StaticJsonDocument<8192> doc;
 
-
-
-
 void webserverSetup(){
     File file = open_file("registers.json", "r");
     read_json_file_to_doc(file, doc);
     
-    
+
     for (JsonPair register_item : doc["registers"].as<JsonObject>()) {       
         // Create /register?nr=1000 like webhook and generate a JSON answer
         server.on("/register", HTTP_GET, [](AsyncWebServerRequest *request) {     
             if (request->args() == 0)
-                return request->send(400, "text/plain", F("ERROR: Bad or no arguments"));      
-            String register_nr = request->arg("nr");         
-            Serial.println("Register nr: " + String(register_nr));
-            String rw = request->arg("rw");
-            String value = request->arg("value");
+                return request->send(400, "text/plain", F("ERROR: Bad or no arguments"));  
+            
+            
+
+            String register_nr  = request->arg("nr");         
+            String rw           = request->arg("rw");
+            String value        = request->arg("value");
+            
+            Serial.println("Value GET is set to: " + value);
             
             AsyncResponseStream *response = request->beginResponseStream("application/json");
             DynamicJsonDocument json(1024);
             
             if (doc["registers"].containsKey(register_nr)) {
-                json["nr"] = register_nr;
-                json["rw"] = doc["registers"][register_nr]["rw"];
-                json["value"] = doc["registers"][register_nr]["value"];
-                json["default"] = doc["registers"][register_nr]["default"];
-                json["description"] =doc["registers"][register_nr]["description"];
+                json["nr"]          = register_nr;
+                json["rw"]          = doc["registers"][register_nr]["rw"];
+                json["value"]       = doc["registers"][register_nr]["value"];
+                json["default"]     = doc["registers"][register_nr]["default"];
+                json["description"] = doc["registers"][register_nr]["description"];
                 serializeJson(json, *response);
                 request->send(response);  	
             }
